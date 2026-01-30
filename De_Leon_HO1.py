5f7e914e456a9ce89a77c3de5393443e355a976e
John Axel De Leon <224013364+JohnAxel04@users.noreply.github.com>
2026-01-30 21:05:30 +0800

    Add files via upload

--- De_Leon_HO1.py
+++ De_Leon_HO1.py
@@ -2,77 +2,48 @@ import time
 
 listKo = []
 
-a = ("Welcome to my Activity\n")
-for text in a:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.5)
+print("Welcome to my Activity")
 
-a1 = ("Named...\n")
-for text in a1:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.3)
 
-a2 = ("Name Or Number\n")
-for text in a2:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.5)
+print("Name Or Number")
 
-a3 = ("\nLets Start\n")
-for text in a3:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.2)
-z = 0
+print("\nLets Start")
 
-a7 = ("\nWhat is you Name? ")
-for text in a7:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.5)
-x = input("\nTyping... ")
+z = 0 #total of the numbers
 
-#to get the length of the input name 
-y = len(x)
+x = input("What is you Name? ")
 
-a8 = (f"\nYour name is '{x}' Hello!\n")
-for text in a8:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.5)
+y = len(x)#to get the length of the input name 
+
+print(f"\nYour name is '{x}' Hello!")
+
 
 #numbers "for loop"
 for i in range(y):
-    tanong = int(input(f"\nPut the Number {i + 1} : ")) # +1 so that the input will start at count of 1 rather than  0
+    tanong = int(input(f"Put the Number {i + 1} : ")) # +1 so that the input will start at count of 1 rather than  0
     listKo.append(tanong)
     z += tanong
 
-z /= i+1 # +1 again because when i saw my first output it looks like over of 1 then i try to put +1 and it became correct 
+def average(name , num):
+    return name / num
+
+aver = average(z , y)
 
 #list
-a6 = (f"\nThis is your Numbers\n")
-for text in a6:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.5)
+print(f"\nThis is your Numbers")
 print(listKo)
 
 #length
-a4 = (f"\nThe Length of your Name is '{y}'")
-for text in a4:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.5)
+print(f"\nThe Length of your Name is '{y}'")
 
 #average
-a5 = (f"\nThe Average of your Numbers is '{z:.2f}'")
-for text in a5:
-    print(text,end="", flush=True)
-    time.sleep(0.07)
-time.sleep(.5)
-if y > z:
-    print(f"\nYour Name '{x}' is greater than the average")
-else:
-    print(f"\nYour Name '{x}' is less than the average")
+print(f"The Average of your Numbers is '{aver:.2f}'")
+
+def com():
+    if y > aver:
+        print(f"Your Name '{x}' is greater than the average")
+    elif y == aver:
+        print(f"Your Name '{x}' is the same as the average\n")
+    else:
+        print(f"Your Name '{x}' is less than the average")
+com()
