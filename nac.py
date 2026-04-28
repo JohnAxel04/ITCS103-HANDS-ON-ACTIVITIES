7d9f5a2bf98d6f01996efa2bc08f94619e077a11
John Axel De Leon <224013364+JohnAxel04@users.noreply.github.com>
2026-04-29 06:15:03 +0800

    Name Age Course Tkinter + Openpyxl

--- nac.py
+++ nac.py
@@ -9,6 +9,22 @@ def submitted():
     sheet = wrk.active
     sheet.append([newname,newage,newcourse])
     wrk.save("File.xlsx")
+def view():
+    top = new.Toplevel(window)
+    top.transient(window)
+    top.grab_set()
+    toplabel = new.Label(top,text="View Saved Files")
+    toplabel.grid(columnspan=3)
+    lbl = new.Label(top,text="Name Age Course")
+    lbl.grid(row=1,columnspan=3)
+    labelview = new.Label(top,width=10)
+    labelview.grid(row=2,columnspan=3)
+    wrkk = op.load_workbook("File.xlsx")
+    sheets = wrkk.active
+    for i in sheets.iter_rows(values_only=True):
+        labelview['text'] = i
+    
+    
 window = new.Tk()
 window.title("Openpyxl")
 mainlabel = new.Label(window,text="Fill up the blank")
@@ -27,4 +43,6 @@ courseEntry = new.Entry(window)
 courseEntry.grid(row=4,column=1,columnspan=2)
 btn = new.Button(window,text="Submit",command=submitted,width=10)
 btn.grid(row=5,column=0,columnspan=4)
+readbtn = new.Button(window,text="View",command=view,width=10)
+readbtn.grid(row=6,columnspan=4)
 window.mainloop()
