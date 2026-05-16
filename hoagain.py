9f7c424cdc55502ae98accdbe2427dbad6bbfbcc
John Axel De Leon <224013364+JohnAxel04@users.noreply.github.com>
2026-05-16 21:53:13 +0800

    done but got error

--- hoagain.py
+++ hoagain.py
@@ -55,6 +55,74 @@ def show():
     for i in sht.iter_rows(min_row=2,values_only=True):
         table.insert("",new.END,values=i)
 
+def select(event):
+    selects = table.focus()
+    values = table.item(selects,"values")
+
+    if values:
+        nameEntry.delete(0,new.END)
+        birthEntry.delete(0,new.END)
+
+        nameEntry.insert(0,values[1])
+        birthEntry.insert(0,values[2])
+
+def update():
+    selected = table.focus()
+    values = table.item(selected,"values")
+
+    if not selected:
+        messagebox.showerror("Not selected","Select first to update data")
+        return
+
+    if not inputvalidation:
+        return
+    
+    lw = op.load_workbook("letsgo.xlsx")
+    shat = lw.active
+
+    ids = values[0]
+    name = nameEntry.get()
+    bth = int(birthEntry.get())
+    age = 2026 - bth
+
+    for row in shat.iter_rows(min_row=2):
+        if int(row[0].value) == int(ids):
+            row[1].value = name
+            row[2].value = bth
+            row[3].value = age
+    lw.save("letsgo.xlsx")
+
+    messagebox.showinfo("Update Successfull","Data updated sucessfully")
+    show()
+
+def deletes():
+    sel = table.focus
+    values = table.item(sel,"values")
+    record_id = values[0]
+
+    if not sel:
+        messagebox.showerror("Input Invalid","Select first to delete data")
+        return
+    
+    confirm = messagebox.askyesnocancel("Confirmation","Are you sure u want to delete?")
+    if not confirm:
+        return
+
+    lwrk = op.load_workbook("letsgo.xlsx")
+    sht = lwrk.active
+
+    for i,row in enumerate(sht.iter_rows(min_row=2),start=2):
+        if int(row[0].value) == int(record_id):
+            sht.delete_rows(i)
+            break
+
+    lwrk.save("letsgo.xlsx")
+    messagebox.showinfo("Delete","Data successfully Deleted")
+    show()
+    
+        
+    
+
 window = new.Tk()
 window.title("Simple User Input")
 titles = new.Label(window,text="User Input")
@@ -76,16 +144,19 @@ birthlabel = new.Label(window,text="BirthYear:")
 birthlabel.grid(column=0,row=2)
 birthEntry = new.Entry(window)
 birthEntry.grid(row=2,column=1,columnspan=2)
-updatebtn = new.Button(window,text="Update")
+updatebtn = new.Button(window,text="Update",command=update)
 updatebtn.grid(column=0,row=4)
 savebtn = new.Button(window,text="Save",command=save)
 savebtn.grid(column=1,row=4)
-delbtn = new.Button(window,text="Delete")
+delbtn = new.Button(window,text="Delete",command=deletes)
 delbtn.grid(column=2,row=4)
 
 table = ttk.Treeview(window,columns=("ID","Full Name","Birth Year","Age","Gender"),show="headings")
 for col in ("ID","Full Name","Birth Year","Age","Gender"):
     table.heading(col,text=col)
 table.grid(row=5,pady=10,columnspan=3)
+
+table.bind("<<TreeviewSelect>>",select)
+
 show()
 window.mainloop()
